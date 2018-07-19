84736e2b78214fe203142ed6f45229b67055fecc
mictsai60229 <mictsa@nlplab.cc>
2018-07-19 16:21:38 +0800

    edit download

--- download.sh
+++ download.sh
@@ -2,3 +2,6 @@ mkdir -p dataset/GloVe
 curl -Lo dataset/GloVe/glove.840B.300d.zip http://nlp.stanford.edu/data/glove.840B.300d.zip
 mkdir -p dataset/fastText
 curl -Lo dataset/fastText/crawl-300d-2M.vec.zip https://s3-us-west-1.amazonaws.com/fasttext-vectors/crawl-300d-2M.vec.zip
+mkdir encoder
+curl -Lo encoder/infersent1.pkl https://s3.amazonaws.com/senteval/infersent/infersent1.pkl
+curl -Lo encoder/infersent2.pkl https://s3.amazonaws.com/senteval/infersent/infersent2.pkl
